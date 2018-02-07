771bb4046bd06196c134109bd62b0b32892f8573
Aaron Mendez <aaronamendez1@gmail.com>
2018-02-07 17:23:08 -0500

    added enemy object create method and optimized code

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,12 @@
-var ctx = document.getElementById("ctx").getContext("2d");
-ctx.font = '30px Arial';
+// fetching canvas
+var canvas = document.getElementById("canvas").getContext("2d");
+// styling canvas
+canvas.font = '30px Arial';
 
-
-
+// playing field
 var message = 'Out of Bounds!'
 var canvasWidth = 500;
 var canvasHeight = 500;
-
 
 // player
 var player = {
@@ -17,15 +17,30 @@ var player = {
 	name: 'P',
 };
 
-// Enemy
-var enemy = {
-	x: 150,
-	spdX: 30,
-	y: 25,
-	spdY: 15,
-	name: 'E',
-};
+// enemy list
+var enemyList = {};
 
+// create enemies
+Enemy('E1', 150, 250, 10, 15, 'E1');
+Enemy('E2', 250, 350, 10, -15, 'E2');
+Enemy('E3', 350, 150, 10, -8, 'E3');
+
+// enemy stats
+function Enemy(id, x, y, spdX, spdY, name) {
+        var enemy = {
+        x: x,
+        spdX: spdX,
+        y: y,
+        spdY: spdY,
+        name: name,
+        id: id,
+
+        };
+
+        enemyList[id] = enemy;
+}
+
+// update interval
 setInterval(update, 50);
 
 // update entity
@@ -34,9 +49,9 @@ function updateEntity(entity) {
         entity.x += entity.spdX;
         entity.y += entity.spdY;
 
-        ctx.fillText(entity.name, entity.x, entity.y);
+        canvas.fillText(entity.name, entity.x, entity.y);
 
-        console.log('hello', entity.x);
+        console.log('hello, world!', entity.x);
        
         if(entity.x < 0 || entity.x > canvasWidth){
                 console.log(message);
@@ -49,11 +64,18 @@ function updateEntity(entity) {
         }
 };
 
-
+// the call for setinterval (update)
 function update(){
+        canvas.clearRect(0, 0, canvasWidth, canvasHeight)
+
+        for (var i in enemyList) {
+                updateEntity(enemyList[i])
+        }
+
         updateEntity(player);
-        updateEntity(enemy);
+
 };
+
 
 
 
