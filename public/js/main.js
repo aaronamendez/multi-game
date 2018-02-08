876cb1fddc05a9d6fae45350dfa1859087917677
Aaron Mendez <aaronamendez1@gmail.com>
2018-02-08 04:09:43 -0500

    fix third entity

--- public/js/main.js
+++ public/js/main.js
@@ -15,14 +15,17 @@ const player = {
 	y: 40,
 	spdY: 5,
 	name: 'P',
-    hp: 100
+    hp: 100,
+    width: 20,
+    height: 20,
+    color: 'green',
 };
 
 // enemy list
 const enemyList = {};
 
 // enemy stats
-Enemy = function (id, x, y, spdX, spdY, name) {
+Enemy = function (id, x, y, spdX, spdY, name, width, height) {
     var enemy = {
     x: x,
     spdX: spdX,
@@ -30,6 +33,9 @@ Enemy = function (id, x, y, spdX, spdY, name) {
     spdY: spdY,
     name: name,
     id: id,
+    width: width,
+    height: height,
+    color: 'red',
 
     };
 
@@ -37,16 +43,31 @@ Enemy = function (id, x, y, spdX, spdY, name) {
 };
 
 //return distance (number)
-getDistanceBetweenEntity = (entity1,entity2) => {     
+getDistanceBetweenEntity = (entity1, entity2) => {     
     var entityX = entity1.x - entity2.x;
     var entityY = entity1.y - entity2.y;
     return Math.sqrt(entityX * entityX + entityY * entityY);
 };
 
 //return if colliding (true/false)
-testCollisionEntity = (entity1,entity2) => {  
-    var distance = getDistanceBetweenEntity(entity1,entity2);
-    return distance < 30;
+testCollisionEntity = (entity1, entity2) => {  
+    var rect1 = {
+        
+        x: entity1.x - entity1.width /2,
+        y: entity1.y - entity1.height /2,
+        width: entity1.width,
+        height: entity1.height,
+    }
+
+    var rect2 = {
+
+        x: entity2.x-entity2.width/2,
+        y: entity2.y-entity2.height/2,
+        width: entity2.width,
+        height: entity2.height,
+    }
+
+    return testCollisionRect(rect1, rect2)
 };
 
 document.onmousemove = (mouse) => {
@@ -55,13 +76,13 @@ document.onmousemove = (mouse) => {
 
     player.x = mouseX;
     player.y = mouseY;
-}
+};
 
 // update entity
 updateEntity = (entity) => {
 
-    drawEntity(entity);
     updateEntityPosition(entity);
+    drawEntity(entity);
 };
 
 updateEntityPosition = (entity) => {
@@ -80,13 +101,24 @@ updateEntityPosition = (entity) => {
         entity.spdY = -entity.spdY;
     }
 
-}
+};
 
+testCollisionRect = (rect1, rect2) => {
+    return rect1.x <= rect2.x+rect2.width 
+        && rect2.x <= rect1.x+rect1.width
+        && rect1.y <= rect2.y + rect2.height
+        && rect2.y <= rect1.y + rect1.height
+};
+
+// creates player
 drawEntity = (entity) => {
     
-    canvas.fillText(entity.name, entity.x, entity.y);
+    canvas.save();
+    canvas.fillStyle = entity.color;
+    canvas.fillRect(entity.x-entity.width/2, entity.y-entity.height/2, entity.width, entity.height-10, 20, 20);
+    canvas.restore();
 
-}
+};
 
 // the call for setinterval (update)
 update = () => {
@@ -120,9 +152,9 @@ update = () => {
 };
 
 // create enemies
-Enemy('E1', 150, 250, 10, 15, 'E1');
-Enemy('E2', 250, 350, 10, -15, 'E2');
-Enemy('E3', 350, 150, 10, -8, 'E3');
+Enemy('E1', 150, 250, 10, 15, 'E1', 30, 30);
+Enemy('E2', 100, 200, 10, -15, 'E2', 40, 20);
+Enemy('E3', 175, 150, 10, 20, 'E3', 50, 10);
 
 // update interval
 setInterval(update, 20);
