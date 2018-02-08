161f55e0f41984948ee1c5211206f6581d365e9d
Aaron <aaronamendez1@gmail.com>
2018-02-07 20:41:17 -0500

    added time and messages

--- public/js/main.js
+++ public/js/main.js
@@ -4,29 +4,25 @@ const canvas = document.getElementById("canvas").getContext("2d");
 canvas.font = '30px Arial';
 
 // playing field
-const message = 'Out of Bounds!'
+var timeWhenGameStarted = Date.now();
 const canvasWidth = 500;
 const canvasHeight = 500;
 
 // player
 const player = {
-	x: 50,
+    x: 50,
 	spdX: 30,
 	y: 40,
 	spdY: 5,
 	name: 'P',
+    hp: 100
 };
 
 // enemy list
 const enemyList = {};
 
-// create enemies
-Enemy('E1', 150, 250, 10, 15, 'E1');
-Enemy('E2', 250, 350, 10, -15, 'E2');
-Enemy('E3', 350, 150, 10, -8, 'E3');
-
 // enemy stats
-function Enemy(id, x, y, spdX, spdY, name) {
+Enemy = function (id, x, y, spdX, spdY, name) {
     var enemy = {
     x: x,
     spdX: spdX,
@@ -41,30 +37,38 @@ function Enemy(id, x, y, spdX, spdY, name) {
 };
 
 //return distance (number)
-function getDistanceBetweenEntity(entity1,entity2) {     
+getDistanceBetweenEntity = (entity1,entity2) => {     
     var entityX = entity1.x - entity2.x;
     var entityY = entity1.y - entity2.y;
     return Math.sqrt(entityX * entityX + entityY * entityY);
 };
 
 //return if colliding (true/false)
-function testCollisionEntity(entity1,entity2) {  
+testCollisionEntity = (entity1,entity2) => {  
     var distance = getDistanceBetweenEntity(entity1,entity2);
     return distance < 30;
 };
 
-// update interval
-setInterval(update, 20);
+document.onmousemove = (mouse) => {
+    var mouseX = mouse.clientX;
+    var mouseY = mouse.clientY; 
+
+    player.x = mouseX;
+    player.y = mouseY;
+}
 
 // update entity
-function updateEntity(entity) {
+updateEntity = (entity) => {
 
+    drawEntity(entity);
+    updateEntityPosition(entity);
+};
+
+updateEntityPosition = (entity) => {
+    
     entity.x += entity.spdX;
     entity.y += entity.spdY;
 
-    canvas.fillText(entity.name, entity.x, entity.y);
-
-    // console.log('hello, world!', entity.x);
        
     if(entity.x < 0 || entity.x > canvasWidth){
         // console.log(message);
@@ -75,10 +79,17 @@ function updateEntity(entity) {
         // console.log(message);
         entity.spdY = -entity.spdY;
     }
-};
+
+}
+
+drawEntity = (entity) => {
+    
+    canvas.fillText(entity.name, entity.x, entity.y);
+
+}
 
 // the call for setinterval (update)
-function update(){
+update = () => {
     canvas.clearRect(0, 0, canvasWidth, canvasHeight)
 
     for (var i in enemyList) {
@@ -87,17 +98,34 @@ function update(){
 
         var isColliding = testCollisionEntity(player, enemyList[i]);
         if (isColliding) {
-            console.log('Colliding!')
+            
+            console.log('Collided!')
+            
+            player.hp = player.hp - 10;
+
+            if (player.hp <= 0) {
+
+                var timeSurvived = Date.now() - timeWhenGameStarted
+                console.log('You lost! You survived for ' + timeSurvived + 'miliseconds.');
+                timeWhenGameStarted = Date.now()
+                player.hp = 100;
+                
+            }
+
         }
     }
 
-    updateEntity(player);
-
+    drawEntity(player);
+    canvas.fillText(player.hp + "HP", 0, 20)
 };
 
+// create enemies
+Enemy('E1', 150, 250, 10, 15, 'E1');
+Enemy('E2', 250, 350, 10, -15, 'E2');
+Enemy('E3', 350, 150, 10, -8, 'E3');
 
-
-
+// update interval
+setInterval(update, 20);
 
 
 
