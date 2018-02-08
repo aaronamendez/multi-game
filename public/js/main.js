a82f323bbce6dfdcb17c2f0c15f10c924389420a
Aaron Mendez <aaronamendez1@gmail.com>
2018-02-07 19:23:37 -0500

    respect

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,15 @@
 // fetching canvas
-var canvas = document.getElementById("canvas").getContext("2d");
+const canvas = document.getElementById("canvas").getContext("2d");
 // styling canvas
 canvas.font = '30px Arial';
 
 // playing field
-var message = 'Out of Bounds!'
-var canvasWidth = 500;
-var canvasHeight = 500;
+const message = 'Out of Bounds!'
+const canvasWidth = 500;
+const canvasHeight = 500;
 
 // player
-var player = {
+const player = {
 	x: 50,
 	spdX: 30,
 	y: 40,
@@ -18,7 +18,7 @@ var player = {
 };
 
 // enemy list
-var enemyList = {};
+const enemyList = {};
 
 // create enemies
 Enemy('E1', 150, 250, 10, 15, 'E1');
@@ -27,52 +27,71 @@ Enemy('E3', 350, 150, 10, -8, 'E3');
 
 // enemy stats
 function Enemy(id, x, y, spdX, spdY, name) {
-        var enemy = {
-        x: x,
-        spdX: spdX,
-        y: y,
-        spdY: spdY,
-        name: name,
-        id: id,
+    var enemy = {
+    x: x,
+    spdX: spdX,
+    y: y,
+    spdY: spdY,
+    name: name,
+    id: id,
 
-        };
+    };
 
-        enemyList[id] = enemy;
-}
+    enemyList[id] = enemy;
+};
+
+//return distance (number)
+function getDistanceBetweenEntity(entity1,entity2) {     
+    var entityX = entity1.x - entity2.x;
+    var entityY = entity1.y - entity2.y;
+    return Math.sqrt(entityX * entityX + entityY * entityY);
+};
+
+//return if colliding (true/false)
+function testCollisionEntity(entity1,entity2) {  
+    var distance = getDistanceBetweenEntity(entity1,entity2);
+    return distance < 30;
+};
 
 // update interval
-setInterval(update, 50);
+setInterval(update, 20);
 
 // update entity
 function updateEntity(entity) {
 
-        entity.x += entity.spdX;
-        entity.y += entity.spdY;
+    entity.x += entity.spdX;
+    entity.y += entity.spdY;
 
-        canvas.fillText(entity.name, entity.x, entity.y);
+    canvas.fillText(entity.name, entity.x, entity.y);
 
-        console.log('hello, world!', entity.x);
+    // console.log('hello, world!', entity.x);
        
-        if(entity.x < 0 || entity.x > canvasWidth){
-                console.log(message);
-                entity.spdX = -entity.spdX;
-        }
+    if(entity.x < 0 || entity.x > canvasWidth){
+        // console.log(message);
+        entity.spdX = -entity.spdX;
+    }
 
-        if(entity.y < 0 || entity.y > canvasHeight){
-                console.log(message);
-                entity.spdY = -entity.spdY;
-        }
+    if(entity.y < 0 || entity.y > canvasHeight){
+        // console.log(message);
+        entity.spdY = -entity.spdY;
+    }
 };
 
 // the call for setinterval (update)
 function update(){
-        canvas.clearRect(0, 0, canvasWidth, canvasHeight)
+    canvas.clearRect(0, 0, canvasWidth, canvasHeight)
 
-        for (var i in enemyList) {
-                updateEntity(enemyList[i])
+    for (var i in enemyList) {
+        
+        updateEntity(enemyList[i])
+
+        var isColliding = testCollisionEntity(player, enemyList[i]);
+        if (isColliding) {
+            console.log('Colliding!')
         }
+    }
 
-        updateEntity(player);
+    updateEntity(player);
 
 };
 
